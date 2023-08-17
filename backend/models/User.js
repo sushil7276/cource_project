1e2c0164216875fa860d458ead0a6e2cac31aac0
Sushil <shil7276@gmail.com>
2023-08-17 18:28:14 +0530

    Comment add

--- backend/models/User.js
+++ backend/models/User.js
@@ -62,20 +62,22 @@ const schema = new mongoose.Schema({
   resetPasswordExpire: String,
 });
 
+// Password save with bcrypt
 schema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  const hashPassword = await bcrypt.hash(this.password, 10);
-  this.password = hashPassword;
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
+// Generate Token
 schema.methods.getJWTToken = function () {
   return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
 };
 
+// Compere Password
 schema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
