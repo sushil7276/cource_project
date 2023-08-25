daa07d9d32e475de39732c629f6ab7ffb56fd50b
Sushil <shil7276@gmail.com>
2023-08-25 15:45:41 +0530

    Create route for add lecture

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -3,7 +3,13 @@ import { createCourse, getAllCourse } from "../controllers/courseController.js";
 
 const router = express.Router();
 
+// Get all course without lectures
 router.route("/courses").get(getAllCourse);
+
+// Create new course - only admin can access this
 router.route("/createcourse").post(createCourse);
+
+// Add lecture, Delete Course, Get Course Details
+router.route("/create/:id").get();
 
 export default router;
