566d1ead38bbc386f81a5362048d874aa577fccd
Sushil <shil7276@gmail.com>
2023-08-28 16:56:11 +0530

    # Create admin authorization

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -6,6 +6,7 @@ import {
   getCourseLectures,
 } from "../controllers/courseController.js";
 import singleUpload from "../middlewares/multer.js";
+import { authorizeAdmin, isAuthentication } from "../middlewares/Auth.js";
 
 const router = express.Router();
 
@@ -13,7 +14,9 @@ const router = express.Router();
 router.route("/courses").get(getAllCourse);
 
 // Create new course - only admin can access this
-router.route("/createcourse").post(singleUpload, createCourse);
+router
+  .route("/createcourse")
+  .post(isAuthentication, authorizeAdmin, singleUpload, createCourse);
 
 // Add lecture, Delete Course, Get Course Details
 router
--- backend/utils/dataUri.js
+++ backend/utils/dataUri.js
@@ -5,7 +5,6 @@ const getDataUri = (file) => {
   const parser = new DataUriParser();
 
   const extName = path.extname(file.originalname).toString();
-  console.log("extName: ", extName);
   return parser.format(extName, file.buffer);
 };
 
