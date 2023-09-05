d5f6f3598ea87a5ea0f14e91062aa005276146d0
Sushil <shil7276@gmail.com>
2023-09-05 12:06:46 +0530

    course filter add

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -6,7 +6,13 @@ import cloudinary from "cloudinary";
 import { Stats } from "../models/Stats.js";
 
 export const getAllCourse = CatchAsyncError(async (req, res, next) => {
-  const course = await Course.find().select("-lectures");
+  const keyword = req.query.keyword || "";
+  const category = req.query.category || "";
+
+  const course = await Course.find({
+    title: { $regex: keyword, $options: "i" },
+    category: { $regex: category, $options: "i" },
+  }).select("-lectures");
 
   res.status(200).json({
     success: true,
