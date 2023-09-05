de3569a827b396b371c25841f800f33575675b75
Sushil <shil7276@gmail.com>
2023-09-05 11:32:57 +0530

    bug fix

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -170,12 +170,12 @@ Course.watch().on("change", async () => {
   const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
 
   const courses = await Course.find({});
-  totalViews = 0;
+
+  let totalViews = 0;
 
   for (let i = 0; i < courses.length; i++) {
     totalViews += courses[i].views;
   }
-
   stats[0].views = totalViews;
   stats[0].createdAt = new Date(Date.now());
 
