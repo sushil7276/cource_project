8bd0f3f8624a5f7473667fcbd3e012555a0ec3dc
Sushil <shil7276@gmail.com>
2023-09-07 16:08:49 +0530

    course Reducer and action created

---
+++ frontend/src/redux/actions/courseAction.js
@@ -0,0 +1,27 @@
+import { server } from '../store';
+import axios from 'axios';
+
+// Update Profile
+export const updateProfile = (name, email) => async dispatch => {
+  try {
+    dispatch({ type: 'updateProfileRequest' });
+
+    const { data } = await axios.put(
+      `${server}/updateprofile`,
+      { name, email },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      }
+    );
+
+    dispatch({ type: 'updateProfileSuccess', payload: data.message });
+  } catch (error) {
+    dispatch({
+      type: 'updateProfileFail',
+      payload: error.response.data.message,
+    });
+  }
+};
