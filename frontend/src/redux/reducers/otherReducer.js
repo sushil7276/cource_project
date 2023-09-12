732f997079743a26faac5a9e32c685f917677e3d
Sushil <shil7276@gmail.com>
2023-09-12 17:51:52 +0530

    contact and course Request Reducer done

---
+++ frontend/src/redux/reducers/otherReducer.js
@@ -0,0 +1,44 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+export const otherReducer = createReducer(
+  {},
+  {
+    // Contact Request
+    contactRequest: state => {
+      state.loading = true;
+    },
+
+    contactSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+
+    contactFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    // Course Request
+    courseRequestRequest: state => {
+      state.loading = true;
+    },
+
+    courseRequestSuccess: (state, action) => {
+      state.loading = false;
+      state.message = action.payload;
+    },
+
+    courseRequestFail: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    clearError: state => {
+      state.error = null;
+    },
+
+    clearMessage: state => {
+      state.message = null;
+    },
+  }
+);
