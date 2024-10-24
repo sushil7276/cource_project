908296c67556985a9ea05da7b2daf09e3fa959b3
Sushil Babar <86038284+sushil7276@users.noreply.github.com>
2024-10-24 14:01:17 +0530

    Update store.js
    change backend url

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -8,7 +8,7 @@ import { courseReducer } from './reducers/courseReducer';
 import { adminReducer } from './reducers/adminReducer';
 import { otherReducer } from './reducers/otherReducer';
 
-export const server = 'https://course-2wm6.onrender.com/api/v1';
+export const server = 'https://cource-project-1.onrender.com/api/v1';
 
 const store = configureStore({
   reducer: {
